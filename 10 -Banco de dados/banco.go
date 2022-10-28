aebf9302732cfb73a62f17b7c4190d167dce1ac7
Gabriel <gabriel@MacBook.local>
2022-10-28 00:38:24 -0700

    Implementacao parte 2

--- 10 -Banco de dados/banco.go
+++ 10 -Banco de dados/banco.go
@@ -1,20 +1,33 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 
-	"gorm.io/gorm"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	db, err := gorm.Open("mysql", "golang:G1ogo@2060@localhost/devbook")
-	if err != nil {
-		log.Fatal(err)
+	stringConexao := "golang:golang@/devbook?charset=utf8&parseTime=True&loc=Local"
+	db, erro := sql.Open("mysql", stringConexao)
+	if erro != nil {
+		log.Fatal(erro)
 	}
 	defer db.Close()
 
-	err = db.Ping()
-	if err != nil {
-		log.Fatal(err)
+	if erro = db.Ping(); erro != nil {
+		log.Fatal(erro)
 	}
+
+	fmt.Println("Conexão está aberta!")
+
+	linhas, erro := db.Query("select * from usuarios")
+	if erro != nil {
+		log.Fatal(erro)
+	}
+	defer linhas.Close()
+
+	fmt.Println(linhas)
+
 }
